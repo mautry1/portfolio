ba2819f505a5b721f64731dc780f2bf0f27f9c00
Micah <micahautry2@gmail.com>
2025-03-03 20:05:04 -0700

    /types update for Project

--- portfolio-frontend/src/types/types.ts
+++ portfolio-frontend/src/types/types.ts
@@ -1,7 +1,9 @@
 export type Project = {
     _id: string;
     title: string;
+    name: string;
     description: string;
+    html_url: string;
     techStack: string[];
     githubLink: string;
     liveDemoLink: string;
